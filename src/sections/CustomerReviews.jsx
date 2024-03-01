880e626d9c50d5ce9818d5dd59900747b172cb8f
Beata Konya <beatakonya@gmail.com>
2024-03-01 21:22:33 +0000

    continued working on testimonials for page

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import ReviewCards from '../components/ReviewCard'; // Assuming ReviewCards component is in the same directory
+import ReviewCards from '../components/ReviewCard';
 const CustomerReviews = () => {
     return (
-      <section id="reviews" className="bg-slate-300">
-        <div className="title">
-          <h2 className='text-[1.7rem] md:[2.2rem] text-center'>See What Our Clients Say...</h2>git
-        </div>
-  
-        <div className="container max-sm:mt-12 flex justify-center items-center">
-          {/* Use ReviewCards component to display review cards */}
-          <ReviewCards />
-        </div>
-      </section>
+        <section id="reviews" className="bg-slate-300">
+            <div className="title">
+                <h2 className='text-[1.7rem] md:[2.2rem] text-center'>See What Our Clients Say...</h2>
+            </div>
+
+            <div className="container max-sm:mt-12 flex justify-center items-center">
+                {/* Use ReviewCards component to display review cards */}
+                <ReviewCards />
+            </div>
+        </section>
     );
-  }
-  
+}
 
 export default CustomerReviews;
