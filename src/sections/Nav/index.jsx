c8ff797741c7194323b27662d9332217f9de27fc
Ramona Cirlan <ramona.cirlan@rendallandrittner.co.uk>
2024-03-01 10:59:08 +0000

    updated the code on App.jsx to import the sections

--- src/sections/Nav/index.jsx
+++ src/sections/Nav/index.jsx
@@ -8,7 +8,7 @@ const MyNav = () => {
     return (
         <>
             <nav>
-                <div className="container mx-auto justify-between md:felx hidden">
+                <div className="container mx-auto justify-between md:felx hidden bg-black">
                     <a href="" className="my-2 flex items-end gap-3">
                         <img src={logo} alt="logo" height={71} width={71} />
                     </a>
