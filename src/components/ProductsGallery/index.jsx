f63731ea03b9faef62856d4bf83bed00780c9a29
raphaelagil <raphaela.gil@yahoo.com>
2024-03-01 09:03:32 -0300

    added ProductsGallery code

--- src/components/ProductsGallery/index.jsx
+++ src/components/ProductsGallery/index.jsx
@@ -1,2 +1,15 @@
-import products from '../../products.json'
-import productCard from '../ProductCard'
+import React from 'react';
+import products from '../../products.json';
+import ProductCard from '../ProductCard'; // Assuming ProductCard is the correct component
+
+const ProjectGallery = () => {
+  return (
+    <>
+      {products.map(product => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </>
+  );
+}
+
+export default ProjectGallery;
