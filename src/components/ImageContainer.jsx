b1ba949747875f8df8ad2f887a5a4ee3d0e3a18a
Ida R <i.jaqueline.reis@gmail.com>
2024-03-01 19:40:42 +0000

    added imagecontainer initial function for IMG API

---
+++ src/components/ImageContainer.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import ImageInspo from './ImageInspo';
+
+
+const ImageContainer = () => {
+    const [imageUrl, setImageUrl] = useState('');
+
+}
