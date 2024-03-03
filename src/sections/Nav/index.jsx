a8cea6e7eabd24b820603c834f839cccf77ad7de
raphaelagil <raphaela.gil@yahoo.com>
2024-03-03 16:44:59 -0300

    added dropdown for products

--- src/sections/Nav/index.jsx
+++ src/sections/Nav/index.jsx
@@ -1,95 +1,94 @@
-
 import React, { useState } from "react";
 import "./style.css";
 
 import logo from "../../assets/images/Logo.jpg";
 
+
 const MyNav = () => {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    // Function to open the dropdown menu
+    const openDropdown = () => {
+    setDropdownOpen(true);
+    };
+    // Function to close the dropdown menu
+    const closeDropdown = () => {
+    setDropdownOpen(false);
+    };
 
-  // Function to toggle the dropdown menu
-  const toggleDropdown = () => {
-    setDropdownOpen(!dropdownOpen);
-  };
-
-  return (
+return (
     <>
-      <nav>
+    <nav>
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-          <div className="relative flex h-16 items-center justify-between">
+        <div className="relative flex h-16 items-center justify-between">
             <div className="flex-shrink-0 items-center">
-              <a href="" className="my-2 flex items-end gap-3">
+            <a href="" className="my-2 flex items-end gap-3">
                 <img src={logo} alt="logo" height={71} width={71} />
-              </a>
+            </a>
             </div>
             <div className="hidden sm:block sm:ml-6">
-              <div className="flex space-x-4"></div>
-              <a
+            <div className="flex space-x-4"></div>
+            <a
                 href="#home"
-                className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium mr-6 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              >
+                className=" text-black rounded-md px-3 py-2 text-sm font-medium mr-6 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            >
                 Home
-              </a>
-              <a
+            </a>
+            <a
                 href="#about"
-                className="bg-gray-900 text-white rounded-md px-3 py-2 mr-6 text-sm font-medium hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              >
+                className=" text-black rounded-md px-3 py-2 mr-6 text-sm font-medium hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            >
                 About us
-              </a>
-              <button
-                onClick={toggleDropdown} // Add onClick handler to toggle dropdown
-                className="bg-gray-900 text-white rounded-md px-3 py-2 mr-6 text-sm font-medium hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              >
+            </a>
+            <button
+                onMouseEnter={openDropdown} // Add onMouseEnter handler to open dropdown
+                onMouseLeave={closeDropdown} // Add onMouseLeave handler to close dropdown
+                className="text-black rounded-md px-3 py-2 mr-6 text-sm font-medium hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            >
                 Products{" "} 
-              </button>
+            </button>
               {/* Conditionally render the dropdown menu based on dropdownOpen state */}
-              {dropdownOpen && (
+            {dropdownOpen && (
                 <div
-                  id="dropdownNavbar"
-                  className="z-10 absolute mt-2 w-48 bg-white divide-y divide-gray-100 rounded-lg shadow-lg dark:bg-gray-700 dark:divide-gray-600"
+            id="dropdownNavbar"
+            className=" absolute mt-2 w-48 bg-white divide-y divide-gray-100 rounded-lg shadow-lg dark:bg-gray-700 dark:divide-gray-600"
+                  onMouseEnter={openDropdown} // Keep dropdown open when hovering over it
+                  onMouseLeave={closeDropdown} // Close dropdown when mouse leaves it
                 >
-                  <ul
+            <ul
                     className="py-2 text-sm text-gray-700 dark:text-gray-400"
                     aria-labelledby="dropdownLargeButton"
-                  >
+                >
                     <li>
-                      <a
+                    <a
                         href="#"
                         className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                      >
+                    >
                         Woman
-                      </a>
+                    </a>
                     </li>
                     <li>
-                      <a
+                    <a
                         href="#"
                         className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                      >
+                    >
                         Men
-                      </a>
-                      </li>
-                  </ul>
+                    </a>
+                    </li>
+                </ul>
                 </div>
-              )}
-              <a
+            )}
+            <a
                 href="#contact"
-                className="bg-gray-900 text-white rounded-md px-3 py-2 mr-6 text-sm font-medium hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              >
+                className=" text-black rounded-md px-3 py-2 mr-6 text-sm font-medium hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            >
                 Contact Us
-              </a>
-              {/* <a
-                href="#favorites"
-                className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium mr-6 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              >
-            Favorites 
-              </a> */}
+            </a>
             </div>
-          </div>
         </div>
-      </nav>
+        </div>
+    </nav>
     </>
-  );
+);
 };
 
 export default MyNav;
-
