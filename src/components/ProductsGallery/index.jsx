fe07395787f2d9194e203335dd2f5c2fa0dd954f
Giada <sponzilli.g@gmail.com>
2024-03-03 12:46:36 +0000

    added category men and women in json and popularity, also added load more button in productgallery

--- src/components/ProductsGallery/index.jsx
+++ src/components/ProductsGallery/index.jsx
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import products from '../../products.json';
-import ProductCard from '../../sections/ProductCard'; // Assuming ProductCard is the correct component
+import ProductCard from '../../sections/ProductCard'; 
 
 import './style.css'
 
 const ProjectGallery = () => {
+  const [displayedProducts, setDisplayedProducts] = useState([]);
+
+  useEffect(() => {
+    const shuffledProducts = products.sort(() => Math.random() - 0.5);
+    const firstEightProducts = shuffledProducts.slice(0, 8);
+    setDisplayedProducts(firstEightProducts);
+  }, []);
+
+  const loadMore = () => {
+    const nextProducts = products.slice(displayedProducts.length, displayedProducts.length + 8);
+    setDisplayedProducts([...displayedProducts, ...nextProducts]);
+  };
+
   return (
     <>
+    <h1 className="flex flex-wrap justify-center">Our Products</h1>
       <div className="flex flex-wrap justify-center">
-        {products.map(product => (
+      {displayedProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
+      {displayedProducts.length < products.length && (
+        <div className="flex justify-center mt-4">
+          <button
+            className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600"
+            onClick={loadMore}
+          >
+            Load More
+          </button>
+        </div>
+      )}
     </>
   );
 }
