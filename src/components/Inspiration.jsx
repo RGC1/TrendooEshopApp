9c4918956bc7df5c39fe88fd135aadc1bb270067
Ida R <i.jaqueline.reis@gmail.com>
2024-03-04 15:57:55 +0000

    added styling to the carousel

--- src/components/Inspiration.jsx
+++ src/components/Inspiration.jsx
@@ -7,35 +7,44 @@ const ImageInspiration = () => {
     const [imageUrls, setImageUrls] = useState([]);
     const apiKey = '6FeaPh0XHiaVxX60hJoMiqxISkh0KrYDF7SQb5svK3UyNWNj4Bwiq6ZC'
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get('https://api.pexels.com/v1/search', {
-                    headers: {
-                        Authorization: apiKey,
-                    },
-                    params: {
-                        query: 'fashion',
-                        per_page: 5,
-                    },
-                });
-                //Extracts random image URL
-                const randomFashionImage = response.data.photos.map(photo => photo.src.original);
-                //Sets the random image to the state
-                setImageUrls(randomFashionImage);
-            } catch (error) {
-                console.error('Error fetching Pexels API:', error);
-            }
-        };
+    const fetchData = async () => {
+        try {
+            const response = await axios.get('https://api.pexels.com/v1/search', {
+                headers: {
+                    Authorization: apiKey,
+                },
+                params: {
+                    query: 'fashion',
+                    per_page: 5,
+                    orientation: 'landscape',
+                },
+            });
+            //Extracts random image URL
+            const randomFashionImage = response.data.photos.map(photo => photo.src.original);
+            //Sets the random image to the state
+            setImageUrls(randomFashionImage);
+        } catch (error) {
+            console.error('Error fetching Pexels API:', error);
+        }
+    };
 
+    useEffect(() => {
         fetchData();
+
+        const intervalId = setInterval(() => {
+            fetchData();
+        }, 5000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
     }, [apiKey]);
 
     return (
         <Carousel>
             {imageUrls.map((url, index) => (
-                <Carousel.Item key={index}>
-                    <img className='d-block w-100' src={url} alt={`Fashion Image ${index}`} />
+                <Carousel.Item key={index} style={{ height: '400px', width: '100%'}}>
+                    <img className='d-block w-100 h-100 object-fit-cover' src={url} alt={`Fashion Image ${index}`} />
                 </Carousel.Item>
             ))}
         </Carousel>    
