c9b6731759589399e9c889cf8873fcfac4879f9a
Beata Konya <beatakonya@gmail.com>
2024-03-06 07:54:27 +0000

    working on code

--- src/context/FavoritesContext.jsx
+++ src/context/FavoritesContext.jsx
@@ -3,6 +3,18 @@ import React, { createContext, useContext, useState } from 'react';
 
 
 
+const FavoritesContext = createContext();
+
+export const FavoritesProvider = ({ children }) => {
+  const [favorites, setFavorites] = useState([]);
+
+  const toggleFavorite = (productId) => {
+    if (favorites.includes(productId)) {
+      setFavorites(favorites.filter((id) => id !== productId));
+    } else {
+      setFavorites([...favorites, productId]);
+    }
+  };
 
 
 
