d1cbc352fe4dbca3f82ba2d651569827ac359750
Giada <sponzilli.g@gmail.com>
2024-03-06 16:38:12 +0000

    added text remove from favorite and added pictures to favorite dropdown

--- src/components/FavoriteList.jsx
+++ src/components/FavoriteList.jsx
@@ -11,12 +11,12 @@ function FavoriteList() {
                 <div className="absolute top-14 right-0 bg-white w-48 mt-2 p-2 rounded-md shadow-md">
                     <h2 className="text-lg font-bold mb-2">Favorited Items</h2>
                     <ul>
-                    {favorites.map((productId) => (
-    <li key={productId}>
-        {}
-        {getFavoriteDetails(productId).name}
-    </li>
-))}
+                        {favorites.map((productId) => (
+                            <li key={productId}>
+                                { }
+                                {getFavoriteDetails(productId).name}
+                            </li>
+                        ))}
                     </ul>
                 </div>
             )}
--- src/sections/Nav/index.jsx
+++ src/sections/Nav/index.jsx
@@ -3,7 +3,7 @@ import logo from "../../assets/images/Logo.png";
 import "./style.css";
 
 
-const MyNav = ({favorites}) => {
+const MyNav = ({ favorites }) => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
   const [displayFavorites, setDisplayFavorites] = useState(false);
@@ -28,10 +28,10 @@ const MyNav = ({favorites}) => {
   };
 
   // Function to display favorited items
-  const DisplayFavoriteItems = (displayFavorites, clickEvent )  => {
-    
+  const DisplayFavoriteItems = (displayFavorites, clickEvent) => {
+
     clickEvent.preventDefault();
-    
+
     setDisplayFavorites(displayFavorites);
   }
 
@@ -102,23 +102,24 @@ const MyNav = ({favorites}) => {
             </div>
             {/* Favorite Button */}
             <a href="" className="text-lg" onClick={(e) => DisplayFavoriteItems(!displayFavorites, e)}>
-        {/* Favorite button icon */}
+              {/* Favorite button icon */}
               <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
                 <path d="M12 21.35l-1.45-1.32C5.4 15.72 2 12.32 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.82-3.4 7.22-8.55 11.53L12 21.35z" />
               </svg>
             </a>
             {/* Display favorited items list */}
-      {displayFavorites && (
-        <div className="absolute top-14 right-0 bg-white w-48 mt-2 p-2 rounded-md shadow-md">
-          <h2 className="text-lg font-bold mb-2">Favorited Items</h2>
-          <ul>
-            {/* Iterate favorited items and display */}
-            {favorites.map((favorite) => (
-              <li key={favorite.id}>{favorite.item}</li>
-            ))}
-          </ul>
-        </div>
-      )}
+            {displayFavorites && (
+              <div className="absolute top-14 right-0 bg-white w-80 mt-2 p-2 rounded-md shadow-md">
+                <h2 className="text-lg font-bold mb-2">Favorited Items</h2>
+                <ul>
+                  {/* Iterate favorited items and display */}
+                  {favorites.map((favorite) => (
+                    <li key={favorite.id} className="flex items-center">
+                      <img src={favorite.image} alt={favorite.item} className="w-16 mr-2 mt-3" /><span className="mt-2">{favorite.item}</span></li>
+                  ))}
+                </ul>
+              </div>
+            )}
             <div className="flex items-center sm:hidden">
               <button
                 onClick={toggleMenu}
@@ -142,8 +143,8 @@ const MyNav = ({favorites}) => {
           </div>
         </div>
       </nav>
-{/* Mobile Menu */}
-<div id='drop'
+      {/* Mobile Menu */}
+      <div id='drop'
         className={`${menuOpen ? "block mobileMenu" : "hidden"
           } sm:hidden`}
       >
