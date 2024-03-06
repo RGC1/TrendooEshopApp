f466ca32cac9e9ccff19225eb3ff1efe679907b2
Beata Konya <beatakonya@gmail.com>
2024-03-06 07:53:52 +0000

    continued code

--- src/context/FavoritesContext.jsx
+++ src/context/FavoritesContext.jsx
@@ -1,2 +1,20 @@
 import React, { createContext, useContext, useState } from 'react';
 // import ProductCard from "../sections/ProductCard";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default FavoritesContext;
